3494d4cfba52deac7ab31cc026afe6e03f408285
jku28 <justinkunews@gmail.com>
2016-08-06 00:24:48 +0100

    Update: added birds.js and users.js

---
+++ birds.js
@@ -0,0 +1,155 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET birdlist
+ *
+ */
+router.get('/birdlist', function(req, res) {
+	var db = req.db;
+	var collection = db.get('birdlist');
+	collection.find({},{},function(e,docs) {
+		res.json(docs);
+	});
+});
+
+/* POST birdlist
+ *
+ */
+router.post('/addbird', function(req,res) {
+	var db = req.db;
+	var collection = db.get('birdlist');
+	collection.insert(req.body, function(err, result) {
+		res.send(
+			(err === null) ? { msg: ''} : { msg: err} 
+		);
+	});
+});
+
+/* 
+ * DELETE to delete bird
+ */
+ router.delete('/deletebird/:id', function(req,res) {
+ 	var db = req.db;
+ 	var collection = db.get('birdlist');
+ 	var birdToDelete = req.params.id;
+ 	collection.remove({ '_id' : birdToDelete }, function(err) {
+ 		res.send((err === null) ? { msg: ''} : { msg:'error: ' + err });
+ 	});
+ });
+
+ 
+/* UPDATE bird
+ *
+ */
+router.get('/updatebird/:id/:vote', function(req,res) {
+	var db = req.db;
+	var collection = db.get('birdlist');
+	var id = req.params.id;
+	var vote = (req.params.vote).toString().toLowerCase();
+	var voteArray = [];
+	// var birdToUpdate;
+	var exists = false;
+	
+	var getBird = function ( callback ) {
+		collection.find({ '_id' : id }, function(e,docs) {
+
+			// birdToUpdate = docs;
+			// console.log(birdToUpdate);
+
+			voteArray = docs[0].votes;
+			callback(voteArray);
+		});
+	}
+
+	var addVotes = function (voteList) {
+		console.log('votelist before loop: ' + voteList);
+
+		var i=0;
+		while (!exists && i<voteList.length) {
+			if (voteList[i][0] == vote) {
+				voteList[i][1] += 1;
+				exists = true;
+			}
+			else { 
+				i++; 
+			}
+		}
+
+		if (!exists) {
+			voteList.push([vote,1]);
+		}	
+
+		// birdToUpdate.votes = voteList;
+		// console.log('bird.votes : ' + birdToUpdate.votes);
+		console.log('votelist after loop: ' + voteList);
+		collection.update({ _id : id }, { $set: { 'votes' : voteList} });
+	}
+
+	getBird(addVotes);
+});
+
+/* GET random bird
+ *
+ */
+router.get('/randomBird', function(req,res) {
+	var db = req.db;
+	var collection = db.get('birdlist');
+
+	// Create weighted array of bird votes, count total votes
+	var birdWeightedList =[];
+	var totalVotes = 0;
+	var maxWeight = 50;
+	var counter = 0;
+
+	var getSightings = function ( callback ) {
+		collection.find({ 'status' : 'unverified' }, function(e,docs){
+
+			// Loops through unverified sightings and performs 4 functions
+			// 1) Get total votes for all unverified sightings
+			// 2) Track maxWeight; Weight = 50-votes/totalVotes; Sighting with lowest votes = greatest weight
+			// 3) Add _id and weight into birdWeightedList array 
+			//    birdWeightedList array is used for fitness proportionate selection  
+			for (var i=0; i<docs.length; i++) {
+				for (var j=0; j<docs[i].votes.length; j++){
+					var votes = 0;
+					votes += docs[i].votes[j][1];
+					totalVotes += docs[i].votes[j][1];
+				}
+
+				if (maxWeight<votes) {
+				maxWeight = votes;
+				}
+
+				birdWeightedList[counter] = [];
+				birdWeightedList[counter][0] = docs[i]._id;
+				birdWeightedList[counter++][1] = 50-votes;
+			}
+			callback (birdWeightedList);
+		});
+	}
+
+	var pickRandomSighting = function (weightedList) {
+		var randomBirdId;
+		var notAccepted = true;
+		var index;
+
+		while (notAccepted){
+			index = Math.round((weightedList.length-1)*Math.random());
+			if (Math.random()<(weightedList[index][1]/maxWeight)) {
+				randomBirdId = weightedList[index][0];
+				notAccepted=false;
+			}
+			else { return; }
+		}
+
+		collection.find({'_id' : randomBirdId},function(e,docs) {
+		res.json(docs);
+		});
+	}
+
+	getSightings( pickRandomSighting );
+
+});
+
+
+module.exports = router;
---
+++ users.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET login - return true || false
+ *
+ */
+
+router.get('/login/:username/:password', function(req,res) {
+	var db = req.db;
+	var collection = db.get('userlist');
+	var username = req.params.username;
+	var password = req.params.password;
+	var usernameExists = false;
+
+	collection.find({ 'username' : username },function(e,docs) {
+
+		// If docs return empty => username does not exist; return false
+		if (docs[0] !== undefined){
+			// If url password = password on file; return true
+			if (docs[0].password === password) {
+				res.send(true);
+			}
+			// If url password != password on file; return false 
+			else { res.send(false); }
+		} 
+		// Return false if password does not match
+		else { res.send(false); }
+
+	});
+});
+
+/* GET change user password
+ *
+ */
+
+router.get('/changepassword/:username/:password', function(req,res) {
+	var db = req.db;
+	var collection = db.get('userlist');
+	var username = req.params.username;
+	var newPassword = req.params.password;
+
+	collection.update({ 'username' : username }, { $set: { 'password' : newPassword} });
+});
+
+
+/* GET userlist
+ *
+ */
+router.get('/userlist', function(req, res) {
+	var db = req.db;
+	var collection = db.get('userlist');
+	collection.find({},{},function(e,docs) {
+		res.json(docs);
+	});
+});
+
+/* POST userlist
+ *
+ */
+router.post('/adduser', function(req,res) {
+	var db = req.db;
+	var collection = db.get('userlist');
+	collection.insert(req.body, function(err, result) {
+		res.send(
+			(err === null) ? { msg: ''} : { msg: err} 
+		);
+	});
+});
+
+/* 
+ * DELETE to delete user
+ */
+ router.delete('/deleteuser/:id', function(req,res) {
+ 	var db = req.db;
+ 	var collection = db.get('userlist');
+ 	var userToDelete = req.params.id;
+ 	collection.remove({ '_id' : userToDelete }, function(err) {
+ 		res.send((err === null) ? { msg: ''} : { msg:'error: ' + err });
+ 	});
+ });
+
+module.exports = router;
